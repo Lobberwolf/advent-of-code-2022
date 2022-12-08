f48e6cb7e58f149052c0b7e10cd0addf507ca7bb
Lobberwolf <lhuecx@gmail.com>
2022-12-08 21:40:32 +0900

    shortened code

--- day-04/main.py
+++ day-04/main.py
@@ -1,31 +1,29 @@
-def get_assignments(filepath:str) -> list:
-    f = open(filepath, "r")
-    lines = f.readlines()
-    assignments = [line.replace("\n","").replace("-",",").split(",") for line in lines]
-    return assignments
+def get_assignments(filepath: str) -> list:
+    lines = open(filepath, "r").readlines()
+    return [l.replace("\n","").replace("-",",").split(",") for l in lines]
 
-def check_assignments(assignments:list, overlap_mode:str) -> list:
-    overlap_assignments = []
-    for assignment in assignments:
-        if overlap_mode == "partial":
-            if int(assignment[0]) >= int(assignment[2]) and int(assignment[0]) <= int(assignment[3]):
-                overlap_assignments.append(assignment)
-            elif int(assignment[1]) >= int(assignment[2]) and int(assignment[1]) <= int(assignment[3]):
-                overlap_assignments.append(assignment)
-            elif int(assignment[2]) >= int(assignment[0]) and int(assignment[2]) <= int(assignment[1]):
-                overlap_assignments.append(assignment)
-            elif int(assignment[3]) >= int(assignment[0]) and int(assignment[3]) <= int(assignment[1]):
-                overlap_assignments.append(assignment)
-        if overlap_mode == "complete":
-            if int(assignment[0]) <= int(assignment[2]) and int(assignment[1]) >= int(assignment[3]):
-                overlap_assignments.append(assignment)
-            elif int(assignment[2]) <= int(assignment[0]) and int(assignment[3]) >= int(assignment[1]):
-                overlap_assignments.append(assignment)
-    return overlap_assignments
+def check_overlaps(assignments: list, mode: str) -> list:
+    overlaps = []
+    for a in assignments:
+        if mode == "partial":
+            if int(a[0]) >= int(a[2]) and int(a[0]) <= int(a[3]):
+                overlaps.append(a)
+            elif int(a[1]) >= int(a[2]) and int(a[1]) <= int(a[3]):
+                overlaps.append(a)
+            elif int(a[2]) >= int(a[0]) and int(a[2]) <= int(a[1]):
+                overlaps.append(a)
+            elif int(a[3]) >= int(a[0]) and int(a[3]) <= int(a[1]):
+                overlaps.append(a)
+        if mode == "complete":
+            if int(a[0]) <= int(a[2]) and int(a[1]) >= int(a[3]):
+                overlaps.append(a)
+            elif int(a[2]) <= int(a[0]) and int(a[3]) >= int(a[1]):
+                overlaps.append(a)
+    return overlaps
 
-def main(filepath:str, overlap_mode:str) -> None:
+def main(filepath: str, mode: str) -> None:
     assignments = get_assignments(filepath)
-    partial_overlap_assignments = check_assignments(assignments, overlap_mode)
+    partial_overlap_assignments = check_overlaps(assignments, mode)
     print(len(partial_overlap_assignments))
 
 if __name__ == "__main__":
