51888641d2179a030cf5309d53c8dba86e0cd542
Lobberwolf <lhuecx@gmail.com>
2022-12-08 21:43:38 +0900

    modified print statement

--- day-04/main.py
+++ day-04/main.py
@@ -5,7 +5,7 @@ def get_assignments(filepath: str) -> list:
 def check_overlaps(assignments: list, mode: str) -> list:
     overlaps = []
     for a in assignments:
-        if mode == "partial":
+        if mode == "Partial":
             if int(a[0]) >= int(a[2]) and int(a[0]) <= int(a[3]):
                 overlaps.append(a)
             elif int(a[1]) >= int(a[2]) and int(a[1]) <= int(a[3]):
@@ -14,7 +14,7 @@ def check_overlaps(assignments: list, mode: str) -> list:
                 overlaps.append(a)
             elif int(a[3]) >= int(a[0]) and int(a[3]) <= int(a[1]):
                 overlaps.append(a)
-        if mode == "complete":
+        if mode == "Complete":
             if int(a[0]) <= int(a[2]) and int(a[1]) >= int(a[3]):
                 overlaps.append(a)
             elif int(a[2]) <= int(a[0]) and int(a[3]) >= int(a[1]):
@@ -24,10 +24,10 @@ def check_overlaps(assignments: list, mode: str) -> list:
 def main(filepath: str, mode: str) -> None:
     assignments = get_assignments(filepath)
     partial_overlap_assignments = check_overlaps(assignments, mode)
-    print(len(partial_overlap_assignments))
+    print(f"{mode} Overlap: {len(partial_overlap_assignments)}")
 
 if __name__ == "__main__":
     FILEPATH = "day-04/puzzle_input.txt"
 
-    main(FILEPATH, "partial")
-    main(FILEPATH, "complete")
+    main(FILEPATH, "Partial")
+    main(FILEPATH, "Complete")
